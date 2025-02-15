d2fcc2f09c75a81330c52c8c98a4e7e737870c56
ajroberts0417 <ajroberts0417@gmail.com>
2025-02-15 16:24:23 -0500

    add some voice options

---
+++ src/video/voices.ts
@@ -0,0 +1,122 @@
+export const voices = {
+    "Aria": {
+      "name": "Aria",
+      "description": null,
+      "id": "9BWtsMINqrJLrRacOk9x",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/9BWtsMINqrJLrRacOk9x/405766b8-1f4e-4d3c-aba1-6f25333823ec.mp3"
+    },
+    "Roger": {
+      "name": "Roger",
+      "description": null,
+      "id": "CwhRBWXzGAHq8TQ4Fs17",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/CwhRBWXzGAHq8TQ4Fs17/58ee3ff5-f6f2-4628-93b8-e38eb31806b0.mp3"
+    },
+    "Sarah": {
+      "name": "Sarah",
+      "description": null,
+      "id": "EXAVITQu4vr4xnSDxMaL",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/EXAVITQu4vr4xnSDxMaL/01a3e33c-6e99-4ee7-8543-ff2216a32186.mp3"
+    },
+    "Laura": {
+      "name": "Laura",
+      "description": null,
+      "id": "FGY2WhTYpPnrIDTdsKH5",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/FGY2WhTYpPnrIDTdsKH5/67341759-ad08-41a5-be6e-de12fe448618.mp3"
+    },
+    "Charlie": {
+      "name": "Charlie",
+      "description": null,
+      "id": "IKne3meq5aSn9XLyUdCD",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/IKne3meq5aSn9XLyUdCD/102de6f2-22ed-43e0-a1f1-111fa75c5481.mp3"
+    },
+    "George": {
+      "name": "George",
+      "description": null,
+      "id": "JBFqnCBsd6RMkjVDRZzb",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/JBFqnCBsd6RMkjVDRZzb/e6206d1a-0721-4787-aafb-06a6e705cac5.mp3"
+    },
+    "Callum": {
+      "name": "Callum",
+      "description": null,
+      "id": "N2lVS1w4EtoT3dr4eOWO",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/N2lVS1w4EtoT3dr4eOWO/ac833bd8-ffda-4938-9ebc-b0f99ca25481.mp3"
+    },
+    "River": {
+      "name": "River",
+      "description": null,
+      "id": "SAz9YHcvj6GT2YYXdXww",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/SAz9YHcvj6GT2YYXdXww/e6c95f0b-2227-491a-b3d7-2249240decb7.mp3"
+    },
+    "Liam": {
+      "name": "Liam",
+      "description": null,
+      "id": "TX3LPaxmHKxFdv7VOQHJ",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/TX3LPaxmHKxFdv7VOQHJ/63148076-6363-42db-aea8-31424308b92c.mp3"
+    },
+    "Charlotte": {
+      "name": "Charlotte",
+      "description": null,
+      "id": "XB0fDUnXU5powFXDhCwa",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/XB0fDUnXU5powFXDhCwa/942356dc-f10d-4d89-bda5-4f8505ee038b.mp3"
+    },
+    "Alice": {
+      "name": "Alice",
+      "description": null,
+      "id": "Xb7hH8MSUJpSbSDYk0k2",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/Xb7hH8MSUJpSbSDYk0k2/d10f7534-11f6-41fe-a012-2de1e482d336.mp3"
+    },
+    "Matilda": {
+      "name": "Matilda",
+      "description": null,
+      "id": "XrExE9yKIg1WjnnlVkGX",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/XrExE9yKIg1WjnnlVkGX/b930e18d-6b4d-466e-bab2-0ae97c6d8535.mp3"
+    },
+    "Will": {
+      "name": "Will",
+      "description": null,
+      "id": "bIHbv24MWmeRgasZH58o",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/bIHbv24MWmeRgasZH58o/8caf8f3d-ad29-4980-af41-53f20c72d7a4.mp3"
+    },
+    "Jessica": {
+      "name": "Jessica",
+      "description": null,
+      "id": "cgSgspJ2msm6clMCkdW9",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/cgSgspJ2msm6clMCkdW9/56a97bf8-b69b-448f-846c-c3a11683d45a.mp3"
+    },
+    "Eric": {
+      "name": "Eric",
+      "description": null,
+      "id": "cjVigY5qzO86Huf0OWal",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/cjVigY5qzO86Huf0OWal/d098fda0-6456-4030-b3d8-63aa048c9070.mp3"
+    },
+    "Chris": {
+      "name": "Chris",
+      "description": null,
+      "id": "iP95p4xoKVk53GoZ742B",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/iP95p4xoKVk53GoZ742B/3f4bde72-cc48-40dd-829f-57fbf906f4d7.mp3"
+    },
+    "Brian": {
+      "name": "Brian",
+      "description": null,
+      "id": "nPczCjzI2devNBz1zQrb",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/nPczCjzI2devNBz1zQrb/2dd3e72c-4fd3-42f1-93ea-abc5d4e5aa1d.mp3"
+    },
+    "Daniel": {
+      "name": "Daniel",
+      "description": null,
+      "id": "onwK4e9ZLuTAKqWW03F9",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/onwK4e9ZLuTAKqWW03F9/7eee0236-1a72-4b86-b303-5dcadc007ba9.mp3"
+    },
+    "Lily": {
+      "name": "Lily",
+      "description": null,
+      "id": "pFZP5JQG7iQjIQuC4Bku",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/pFZP5JQG7iQjIQuC4Bku/89b68b35-b3dd-4348-a84a-a3c13a3c2b30.mp3"
+    },
+    "Bill": {
+      "name": "Bill",
+      "description": null,
+      "id": "pqHfZKP75CvOlQylNhV4",
+      "preview_url": "https://storage.googleapis.com/eleven-public-prod/premade/voices/pqHfZKP75CvOlQylNhV4/d782b3ff-84ba-4029-848c-acf01285524d.mp3"
+    }
+}
